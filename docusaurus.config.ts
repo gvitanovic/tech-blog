53f48f4eb2a5c94df132302bdd2d3af1021a1993
gvitanovic <gajaposlovno@gmail.com>
2025-10-23 23:31:00 +0200

    fix: add docsau config for gh-actions

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'gvitanovic', // Usually your GitHub org/user name.
   projectName: 'tech-blog', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
 
